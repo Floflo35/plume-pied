b8127ec3d98c2057ea694870a0f6958b94afc11c
pdesiles <pdesiles@luccasoftware.com>
2024-01-26 23:00:57 +0100

    début gravité

--- Scenes/plume.gd
+++ Scenes/plume.gd
@@ -1,11 +1,35 @@
-extends Area2D
+extends CharacterBody2D
 
+@export var gravity = 900.0
+@export var jump_force = -300.0
 
-# Called when the node enters the scene tree for the first time.
+var max_speed = 400
+var is_active = true
+
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2.ZERO
+ 
+func _physics_process(delta):
+	if !is_active:
+		return
+		
+	if Input.is_action_just_pressed("up"):
+		jump()
+		
+	velocity.y += gravity * delta
+	velocity.y = min(velocity.y , max_speed)
+	
+	move_and_collide(velocity * delta)
 
+func jump():
+	velocity.y = jump_force
+	velocity.x = deg_to_rad(-30)
+	
+	
+func kill():
+	is_active = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func stop():
+	gravity = 0
+	velocity = Vector2.ZERO
+	kill()
