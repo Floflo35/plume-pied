42660e206c9d61b28c183b557119ae8ee87f855e
Florent Chardevel <fchardevel@gmail.com>
2024-01-27 12:37:39 +0100

    mouvement dans les nodes

--- Scenes/chatouilles/pizza.gd
+++ Scenes/chatouilles/pizza.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+@export var speed = -150
+
 func _ready():
 	GameLogic.register_chatouille(self)
 
 func _process(delta):
-	pass
+	position.x += speed * delta
 
 func _on_body_entered(body):
-	GameLogic.chatouille_hit(body);
+	GameLogic.chatouille_hit(body)
