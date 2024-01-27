f795c0df7345229bfd4cf3d218b46fbfde00f712
pdesiles <pdesiles@luccasoftware.com>
2024-01-27 10:15:49 +0100

    add global gamelogic

--- game_logic.gd
+++ game_logic.gd
@@ -1,6 +1,4 @@
-extends Area2D
-
-signal obstacle_hit
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_body_entered(body):
-	obstacle_hit.emit(body);
