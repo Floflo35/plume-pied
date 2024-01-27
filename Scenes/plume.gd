b685bb13d29d83f29bc0568666fac3566af8543e
pdesiles <pdesiles@luccasoftware.com>
2024-01-27 21:44:08 +0100

    extract "fly" command from plume

--- Scenes/plume.gd
+++ Scenes/plume.gd
@@ -5,23 +5,29 @@ extends CharacterBody2D
 
 var max_speed = 400
 var is_active = true
+var fly_on_next_physic_frame = false
 
 func _ready():
 	GameLogic.hit.connect(hit)
 	GameLogic.chatouille.connect(chatouille)
+	GameLogic.fly.connect(fly_requested)
 	velocity = Vector2.ZERO
  
 func _physics_process(delta):
 	if !is_active:
 		return
 		
-	if Input.is_action_just_pressed("up"):
+	if fly_on_next_physic_frame:
+		fly_on_next_physic_frame = false
 		jump()
 		
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y , max_speed)
 	
 	move_and_collide(velocity * delta)
+
+func fly_requested():
+	fly_on_next_physic_frame = true
 
 func jump():
 	velocity.y = jump_force
