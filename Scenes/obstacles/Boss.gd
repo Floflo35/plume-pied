89aa8da76ebf99997fe62ced9f24cb577dc091bc
Florent Chardevel <fchardevel@gmail.com>
2024-01-28 13:20:44 +0100

    boss fight v1

--- Scenes/obstacles/Boss.gd
+++ Scenes/obstacles/Boss.gd
@@ -1,7 +1,56 @@
 extends Node2D
 
 @export var phase1_rotation_speed = 1.0
+var rotation_speed = 0.5
+var score = 0
+
+
+func _ready():
+	rotation_speed = phase1_rotation_speed
+
 
 func _process(delta):
-	$Wheel.rotation += delta * phase1_rotation_speed
-	$BossGuy.rotation += delta * phase1_rotation_speed
+	$Wheel.rotation += delta * rotation_speed
+	$BossGuy.rotation += delta * rotation_speed
+	if score == 4:
+		GameLogic.end_game()
+
+
+func _on_tickle_0_body_entered(body):
+	print("TICKLE")
+	rotation_speed = rotation_speed * 1.15
+	$BossGuy/Tickle0.set_deferred("monitoring", false)
+	$BossGuy/Tickle0.set_deferred("visible", false)
+	GameLogic.chatouille_hit(body)
+	score += 1
+
+
+func _on_tickle_1_body_entered(body):
+	print("TICKLE")
+	rotation_speed = rotation_speed * 1.15
+	$BossGuy/Tickle1.set_deferred("monitoring", false)
+	$BossGuy/Tickle1.set_deferred("visible", false)
+	GameLogic.chatouille_hit(body)
+	score += 1
+
+
+func _on_tickle_2_body_entered(body):
+	print("TICKLE")
+	rotation_speed = rotation_speed * 1.15
+	$BossGuy/Tickle2.set_deferred("monitoring", false)
+	$BossGuy/Tickle2.set_deferred("visible", false)
+	GameLogic.chatouille_hit(body)
+	score += 1
+
+
+func _on_tickle_3_body_entered(body):
+	print("TICKLE")
+	rotation_speed = rotation_speed * 1.15
+	$BossGuy/Tickle3.set_deferred("monitoring", false)
+	$BossGuy/Tickle3.set_deferred("visible", false)
+	GameLogic.chatouille_hit(body)
+	score += 1
+
+
+func _on_hitbox_body_entered(body):
+	GameLogic.obstacle_hit(body)
