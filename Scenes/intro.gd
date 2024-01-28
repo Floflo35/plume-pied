0d0eace2a249f0578ec9ad87af1743158dcf3481
pdesiles <pdesiles@luccasoftware.com>
2024-01-28 12:57:30 +0100

    fade in intro

--- Scenes/intro.gd
+++ Scenes/intro.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _process(delta):
-	if Input.is_action_just_pressed("enter"):
+	if Input.is_action_just_pressed("enter") || Input.is_action_just_pressed("up"):
 		GameLogic.start_game()
 
 func _on_button_button_up():
